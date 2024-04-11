cf1d827625a0f11c3fedfdc51a655f6e2e3adedb
bandan112 <138823706+bandan112@users.noreply.github.com>
2024-04-11 14:28:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.autoTyping = false;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
-global.autoreadgc = true;
+global.autoreadgc = false;
 
 
 //
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
+global.Owner = ["918101400579"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["918101400579"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["918101400579"];
+global.OwnerName = "Ayan";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.author = "By: Ayan";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
